13608d5eb1e57cfa942d2528f25a0a24b38d162d
chinbor <chinbor.Mn@gmail.com>
2022-11-19 17:00:41 +0800

    feat: add selectionSort

---
+++ examples/selection-sort.ts
@@ -0,0 +1,20 @@
+import { swap } from './heap'
+import { Compare, defaultCompare } from './sortedLinkedList'
+
+export const selectionSort = (array: any[], compareFn = defaultCompare) => {
+  const { length } = array
+  let indexMin
+
+  for (let i = 0; i < length - 1; i++) {
+    indexMin = i
+
+    for (let j = i; j < length; j++) {
+      if (compareFn(array[indexMin], array[j]) === Compare.BIGGER_THAN)
+        indexMin = j
+    }
+    if (i !== indexMin)
+      swap(array, i, indexMin)
+  }
+
+  return array
+}
--- test/sorts.test.ts
+++ test/sorts.test.ts
@@ -1,11 +1,27 @@
 import { describe, expect, test } from 'vitest'
 import { modifiedBubbleSort } from '../examples/bubble-sort'
+import { selectionSort } from '../examples/selection-sort'
 
 describe('Sorts', () => {
-  const arr = [8, 7, 5, 4, 3, 2]
-
   test('bubbleSort', () => {
+    const arr = [8, 7, 5, 4, 3, 2]
+
     expect(modifiedBubbleSort(arr)).toMatchInlineSnapshot(`
+      [
+        2,
+        3,
+        4,
+        5,
+        7,
+        8,
+      ]
+    `)
+  })
+
+  test('selectionSort', () => {
+    const arr = [8, 7, 5, 4, 3, 2]
+
+    expect(selectionSort(arr)).toMatchInlineSnapshot(`
       [
         2,
         3,
