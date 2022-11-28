1ec20dc459728898f8951632b07eff62c39d11af
chinbor <chinbor.Mn@gmail.com>
2022-11-28 12:45:07 +0800

    fix: add comments

--- examples/merge-sort.ts
+++ examples/merge-sort.ts
@@ -6,9 +6,13 @@ function merge<T>(left: T[], right: T[], compareFn: ICompareFunction<T>) {
   let j = 0
   const result = []
 
+  // 注意这里使用 i j 进行比较 left数组与right数组 中的元素
   while (i < left.length && j < right.length)
     result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++])
 
+  // 判断是left先遍历完毕还是right，
+  // 1. 若为right先遍历完毕也就是此时 i < left.length 那么说明left余下部分都是大于right中元素
+  // 2. 若为left先遍历完毕也就是此时 i >= left.length 那么说明right余下部分都是大于left中元素
   return result.concat(i < left.length ? left.slice(i) : right.slice(j))
 }
 
